30f6d84f034e4127ef1ccd75bb1e161b90c86436
txzh007 <txzh007@gmail.com>
2019-04-08 16:16:00 +0800

    first commit

---
+++ src/main/java/link/tanxin/wechatpublic/WechatpublicApplication.java
@@ -0,0 +1,27 @@
+package link.tanxin.wechatpublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/***
+ * @author Tan
+ * 2019年4月4日 14:00:17
+ */
+
+@SpringBootApplication
+public class WechatpublicApplication
+//        extends SpringBootServletInitializer
+{
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(WechatpublicApplication.class);
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatpublicApplication.class, args);
+    }
+
+}
