9ad0010ed5a130b89d20ab1c6eb2de692f1f82d3
txzh007 <txzh007@gmail.com>
2019-04-12 00:37:54 +0800

    update wechat token timetask

--- src/main/java/link/tanxin/wechatpublic/utils/ScheduledTask.java
+++ src/main/java/link/tanxin/wechatpublic/utils/ScheduledTask.java
@@ -1,12 +1,17 @@
 package link.tanxin.wechatpublic.utils;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,25 +21,34 @@ import java.util.Map;
  * @author Tan
  * 2019年4月11日 15:41:17
  */
+@Component
 @Configuration
 @EnableScheduling
 public class ScheduledTask {
+
     final String tokenUrl = "https://api.weixin.qq.com/cgi-bin/token";
+    static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    @Value("${wechat.appid}")
+    private String appid;
+    @Value("${wechat.appsecret}")
+    private String appsecret;
 
     /**
-     * 每90分钟获取一次token，Token有效期 2小时
+     * 每90分钟获取一次Token，Token有效期 2小时
      */
     @PostConstruct
-    @Scheduled(cron = "0 0/90 * * * ?")  // 秒 分 时 日 月 周几
+    @Scheduled(cron = "0 0/30 * * * ?")  // 秒 分 时 日 月 周几
     private void timeToGetToken() {
+
         Map<String, Object> map = new HashMap<String, Object>(3);
         map.put("grant_type", "client_credential");
-        map.put("appid", "wxbdc9e3e2a67f8d8e");
-        map.put("secret", "63dff4b49392404865a3f54750636fa2");
-
+        map.put("appid", appid);
+        map.put("secret", appsecret);
+        System.out.println("appid:" + appid);
         System.out.println("***********************************");
         System.out.println(HttpUtils.sendGet(tokenUrl, map, "UTF-8"));
-        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now().format(DATETIME_FORMATTER));
+
 
     }
 }
